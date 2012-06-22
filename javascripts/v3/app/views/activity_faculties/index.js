9eb49bbe3d9da40e91d1acf54487d3598e9e8f63
slamkajs <jslamka5685@gmail.com>
2012-06-22 14:52:39 -0400

    UPDATED:  Added content to js/v3/controllers/activity_faculties and js/v3/view/activity_faculties/index

--- javascripts/v3/app/views/activity_faculties/index.js
+++ javascripts/v3/app/views/activity_faculties/index.js
@@ -1,1 +1,10 @@
+$.Class("ccpd.activity_faculties.index",{
+	init:function(controller,action) {
+		this.controller = new ccpd[controller](action);
+		console.log('in');
+	}
+});
 
+(function(){
+	
+})();
