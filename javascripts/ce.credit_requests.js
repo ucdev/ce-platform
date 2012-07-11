85ebfe7e26b7e7c6ff7f8813b5eaa4d3919e30bd
Joshua Rountree <joshua@swodev.com>
2012-07-11 10:54:09 -0400

    sdfadsf

--- javascripts/ce.credit_requests.js
+++ javascripts/ce.credit_requests.js
@@ -8,7 +8,7 @@
  * @module: credit_requests
  * @def: Model
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.model=f.Model.extend({})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.model=f.Model.extend({})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -19,7 +19,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.model=f.Model.ext
  * @module: credit_requests
  * @def: Collection
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.collection=f.Collection.extend({url:"/api/credit_requests",model:this.model})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.collection=f.Collection.extend({url:"/api/credit_requests",model:this.model})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -30,7 +30,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.collection=f.Coll
  * @module: credit_requests
  * @def: Controller
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.controller=(function(){function g(){var h;this.collection=new a.collection;h=c("#app");this.collection.reset(h.data("credit_requests"));window.col=this.collection;h.removeData("credit_requests")}g.prototype.indexView=function(){var h;h=new a.views.indexView({collection:this.collection});return d.mainRegion.show(h)};g.prototype.editView=function(j){var i,h;i=this.collection.get(j);h=new a.views.editView({model:i});return d.mainRegion.show(h)};g.prototype.showView=function(j){var i,h;i=this.collection.get(j);h=new a.views.showView({model:i});return d.mainRegion.show(h)};return g})()});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.controller=(function(){function g(){var h;this.collection=new a.collection;h=c("#app");this.collection.reset(h.data("credit_requests"));window.col=this.collection;h.removeData("credit_requests")}g.prototype.indexView=function(){var h;h=new a.views.indexView({collection:this.collection});return d.mainRegion.show(h)};g.prototype.editView=function(j){var i,h;i=this.collection.get(j);h=new a.views.editView({model:i});return d.mainRegion.show(h)};g.prototype.showView=function(j){var i,h;i=this.collection.get(j);h=new a.views.showView({model:i});return d.mainRegion.show(h)};return g})()});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -41,7 +41,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.controller=(funct
  * @module: credit_requests
  * @def: Router
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.router=f.Marionette.AppRouter.extend({appRoutes:{"":"index","credit_requests/:id":"show","credit_requests/:id":"edit"}})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.router=f.Marionette.AppRouter.extend({appRoutes:{"":"index","credit_requests/:id":"show","credit_requests/:id":"edit"}})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -52,7 +52,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.router=f.Marionet
  * @module: credit_requests
  * @def: views.rowView
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.rowView=f.Marionette.ItemView.extend({tagName:"tr",template:"credit_requests/row",events:{"click .destroy":"destroy","click .edit":"edit"},destroy:function(){},edit:function(){}})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.rowView=f.Marionette.ItemView.extend({tagName:"tr",template:"credit_requests/row",events:{"click .destroy":"destroy","click .edit":"edit"},destroy:function(){},edit:function(){}})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -63,7 +63,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views|
  * @module: credit_requests
  * @def: views.indexView
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.indexView=f.Marionette.CompositeView.extend({tagName:"table",itemView:this.views.rowView,template:"credit_requests/index",events:{"click .new":"newRecord"},appendHtml:function(g,h){g.$(".credit_requests").append(h.el)},newRecord:function(){}})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.indexView=f.Marionette.CompositeView.extend({tagName:"table",itemView:this.views.rowView,template:"credit_requests/index",events:{"click .new":"newRecord"},appendHtml:function(g,h){g.$(".credit_requests").append(h.el)},newRecord:function(){}})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -74,7 +74,7 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views|
  * @module: credit_requests
  * @def: views.editView
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.editView=f.Marionette.ItemView.extend({template:"credit_requests/edit",})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.editView=f.Marionette.ItemView.extend({template:"credit_requests/edit",})});
 /*!
  * CE Platform
  * http://ccpd.uc.edu/
@@ -85,4 +85,4 @@ CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views|
  * @module: credit_requests
  * @def: views.showView
  */
-CE.module("Modules.credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.showView=f.Marionette.ItemView.extend({template:"credit_requests/show",})});
+CE.module("credit_requests",function(a,d,f,e,c,b){this.views=this.views||{};this.views.showView=f.Marionette.ItemView.extend({template:"credit_requests/show",})});
